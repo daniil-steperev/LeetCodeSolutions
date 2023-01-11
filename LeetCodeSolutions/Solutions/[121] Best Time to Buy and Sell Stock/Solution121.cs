e16b0af7042276ae3260346aaf2fba71f9b0dabf
Daniil Stepyrev <danik-steperev@mail.ru>
2023-01-12 00:21:41 +0300

    Added solution and test for task [121] Best Time to Buy and Sell Stock

---
+++ LeetCodeSolutions/Solutions/[121] Best Time to Buy and Sell Stock/Solution121.cs
@@ -0,0 +1,34 @@
+﻿namespace LeetCodeSolutions.Solutions.BestTimeToBuyAndSellStock
+{
+  public class Solution121
+  {
+    public int MaxProfit(int[] prices)
+    {
+      int maxProfit = 0;
+      int bestBuyPrice = prices[0];
+      int bestSellPrice = 0;
+
+      for (int i = 1; i < prices.Length; i++)
+      {
+        if (i != prices.Length - 1 && prices[i] < bestBuyPrice)
+        {
+          bestBuyPrice = prices[i];
+          bestSellPrice = 0;
+          continue;
+        }
+
+        if (prices[i] > bestBuyPrice && prices[i] > bestSellPrice)
+        {
+          bestSellPrice = prices[i];
+        }
+
+        if (bestSellPrice - bestBuyPrice > maxProfit)
+        {
+          maxProfit = bestSellPrice - bestBuyPrice;
+        }
+      }
+
+      return maxProfit;
+    }
+  }
+}
---
+++ LeetCodeTests/Tests/[121] Best Time to Buy and Sell Stock/Solution121Tester.cs
@@ -0,0 +1,53 @@
+﻿using LeetCodeSolutions.Solutions.BestTimeToBuyAndSellStock;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCodeTests.Tests._121__Best_Time_to_Buy_and_Sell_Stock
+{
+  [TestClass]
+  public class Solution121Tester : AbstractSolutionTester<Solution121>
+  {
+    [TestMethod]
+    public void ProfitBuyAndStockTest() 
+    {
+      int[] prices = { 7, 1, 5, 3, 6, 4 };
+      int maxProfit = 5;
+
+      int methodProfit = solution.MaxProfit(prices);
+
+      Assert.AreEqual(maxProfit, methodProfit);
+    }
+
+    [TestMethod]
+    public void NonProfitBuyAndStockTest()
+    {
+      int[] prices = { 7, 6, 4, 3, 1 };
+      int maxProfit = 0;
+
+      int methodProfit = solution.MaxProfit(prices);
+
+      Assert.AreEqual(maxProfit, methodProfit);
+    }
+
+    [TestMethod]
+    public void ProfitShortBuyAndStockTest()
+    {
+      int[] prices = { 2, 4, 1 };
+      int maxProfit = 2;
+
+      int methodProfit = solution.MaxProfit(prices);
+
+      Assert.AreEqual(maxProfit, methodProfit);
+    }
+
+    [TestMethod]
+    public void ProfitLongBuyAndStockTest()
+    {
+      int[] prices = { 3, 2, 6, 5, 0, 3 };
+      int maxProfit = 4;
+
+      int methodProfit = solution.MaxProfit(prices);
+
+      Assert.AreEqual(maxProfit, methodProfit);
+    }
+  }
+}
