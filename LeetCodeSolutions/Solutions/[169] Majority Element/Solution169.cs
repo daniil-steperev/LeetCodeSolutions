efca00b166de542941f9febbaad043fca610a72d
Daniil Stepyrev <danik-steperev@mail.ru>
2023-01-15 00:16:05 +0300

    Added solution and test of 169 LeetCode task for finding the Majority Element

---
+++ LeetCodeSolutions/Solutions/[169] Majority Element/Solution169.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCodeSolutions.Solutions.MajorityElement
+{
+  public class Solution169
+  {
+    public int MajorityElement(int[] nums)
+    {
+      IDictionary<int, int> elementsHashMap = new Dictionary<int, int>();
+      foreach (int num in nums)
+      {
+        if (elementsHashMap.ContainsKey(num))
+        {
+          elementsHashMap[num]++;
+        }
+        else
+        {
+
+          elementsHashMap.Add(num, 1);
+        }
+      }
+
+      foreach (KeyValuePair<int, int> countedElement in elementsHashMap)
+      {
+        int correctQuantity = (nums.Length + 1) / 2;
+        if (countedElement.Value >= correctQuantity)
+        {
+          return countedElement.Key;
+        }
+      }
+
+      return -1;
+    }
+  }
+}
---
+++ LeetCodeTests/Tests/[169] Majority Element/Solution169Tester.cs
@@ -0,0 +1,31 @@
+﻿using LeetCodeSolutions.Solutions.MajorityElement;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCodeTests.Tests.MajorityElement
+{
+  [TestClass]
+  public class Solution169Tester : AbstractSolutionTester<Solution169>
+  {
+    [TestMethod]
+    public void ShortMajorityElementTest()
+    {
+      int[] nums = { 3, 2, 3, };
+      int majorityElement = 3;
+
+      int methodMajorityElement = solution.MajorityElement(nums);
+
+      Assert.AreEqual(majorityElement, methodMajorityElement);
+    }
+
+    [TestMethod]
+    public void BigMajorityElementTest()
+    {
+      int[] nums = { 2, 2, 1, 1, 1, 2, 2, };
+      int majorityElement = 2;
+
+      int methodMajorityElement = solution.MajorityElement(nums);
+
+      Assert.AreEqual(majorityElement, methodMajorityElement);
+    }
+  }
+}
