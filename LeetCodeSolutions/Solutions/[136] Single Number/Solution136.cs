d948fe5aec9203136409cb4b24a3eb8889974194
Daniil Stepyrev <danik-steperev@mail.ru>
2023-01-16 23:06:30 +0300

    Solver 136 LeetCode task: Single Number. Added solution and test

---
+++ LeetCodeSolutions/Solutions/[136] Single Number/Solution136.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCodeSolutions.Solutions.SingleNumber
+{
+  public class Solution136
+  {
+    public int SingleNumber(int[] nums)
+    {
+      int numberAccumulator = nums[0];
+
+      for (int i = 1; i < nums.Length; i++)
+      {
+        numberAccumulator ^= nums[i];
+      }
+
+      return numberAccumulator;
+    }
+  }
+}
---
+++ LeetCodeTests/Tests/[136] Single Number/Solution136Tester.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCodeSolutions.Solutions.SingleNumber;
+
+namespace LeetCodeTests.Tests.SingleNumber
+{
+  [TestClass]
+  public class Solution136Tester : AbstractSolutionTester<Solution136>
+  {
+    [TestMethod]
+    public void SmallSingleNumberTest()
+    {
+      int[] testNums = { 2, 2, 1 };
+      int singleNumber = 1;
+
+      int methodSingleNumber = solution.SingleNumber(testNums);
+
+      Assert.AreEqual(singleNumber, methodSingleNumber);
+    }
+
+    [TestMethod]
+    public void BigSingleNumberTest()
+    {
+      int[] testNums = { 4, 1, 2, 1, 2 };
+      int singleNumber = 4;
+
+      int methodSingleNumber = solution.SingleNumber(testNums);
+
+      Assert.AreEqual(singleNumber, methodSingleNumber);
+    }
+  }
+}
