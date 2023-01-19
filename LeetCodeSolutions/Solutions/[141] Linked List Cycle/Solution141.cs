6ac718562cabda0b9394d336cc5322259d2287f5
Daniil Stepyrev <danik-steperev@mail.ru>
2023-01-19 23:33:40 +0300

    Added solution 141 and test

---
+++ LeetCodeSolutions/Solutions/[141] Linked List Cycle/Solution141.cs
@@ -0,0 +1,25 @@
+﻿using LeetCodeSolutions.Solutions.RemoveDuplcatesFromSortedList;
+
+namespace LeetCodeSolutions.Solutions.LinkedListCycle
+{
+  public class Solution141
+  {
+    private const int CheckedValue = -1000000;
+
+    public bool HasCycle(ListNode head)
+    {
+      while (head != null)
+      {
+        if (head.val == CheckedValue)
+        {
+          return true;
+        }
+
+        head.val = CheckedValue;
+        head = head.next;
+      }
+
+      return false;
+    }
+  }
+}
---
+++ LeetCodeTests/Tests/[141] Linked List Cycle/Solution141Tester.cs
@@ -0,0 +1,72 @@
+﻿using LeetCodeSolutions.Solutions.LinkedListCycle;
+using LeetCodeSolutions.Solutions.RemoveDuplcatesFromSortedList;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCodeTests.Tests._141__Linked_List_Cycle
+{
+  [TestClass]
+  public class Solution141Tester : AbstractSolutionTester<Solution141>
+  {
+    [TestMethod]
+    public void BigCycleTest()
+    {
+      bool hasCycle = true;
+      ListNode head = new ListNode()
+      {
+        val = 3,
+      };
+
+      head.next = new ListNode()
+      {
+        val = 2,
+        next = new ListNode()
+        {
+          val = 0,
+          next = new ListNode()
+          {
+            val = -4,
+            next = head,
+          }
+        }
+      };
+
+      bool methodOutput = solution.HasCycle(head);
+
+      Assert.AreEqual(hasCycle, methodOutput);
+    }
+
+    [TestMethod]
+    public void SmallCycleTest()
+    {
+      bool hasCycle = true;
+      ListNode head = new ListNode()
+      {
+        val = 1,
+      };
+
+      head.next = new ListNode()
+      {
+        val = 2,
+        next = head,
+      };
+
+      bool methodOutput = solution.HasCycle(head);
+
+      Assert.AreEqual(hasCycle, methodOutput);
+    }
+
+    [TestMethod]
+    public void NoCycleTest()
+    {
+      bool hasCycle = false;
+      ListNode head = new ListNode()
+      {
+        val = 1,
+      };
+
+      bool methodOutput = solution.HasCycle(head);
+
+      Assert.AreEqual(hasCycle, methodOutput);
+    }
+  }
+}
