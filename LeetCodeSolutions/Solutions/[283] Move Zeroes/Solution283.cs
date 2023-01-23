6fce05ee0ae2152caf442bb7d4e13624bb6fd378
Daniil Stepyrev <danik-steperev@mail.ru>
2023-01-23 19:40:00 +0300

    Added test and soluton for task 283: Move zeros

---
+++ LeetCodeSolutions/Solutions/[283] Move Zeroes/Solution283.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCodeSolutions.Solutions._283__Move_Zeroes
+{
+  public class Solution283
+  {
+    public void MoveZeroes(ref int[] nums)
+    {
+      int snowBallSize = 0;
+
+      for (int i = 0; i < nums.Length; i++)
+      {
+        if (nums[i] == 0)
+        {
+          snowBallSize++;
+          continue;
+        }
+
+        if (snowBallSize > 0)
+        {
+          nums[i - snowBallSize] = nums[i];
+          nums[i] = 0;
+        }
+      }
+    }
+  }
+}
---
+++ LeetCodeTests/Tests/[283] Move Zeroes/Solution283Tester.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCodeSolutions.Solutions._283__Move_Zeroes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCodeTests.Tests._283__Move_Zeroes
+{
+  [TestClass]
+  public class Solution283Tester : AbstractSolutionTester<Solution283>
+  {
+    [TestMethod]
+    public void BigZerosTest()
+    {
+      int[] notSortedNums = { 0, 1, 0, 3, 12 };
+      int[] sortedNums = { 1, 3, 12, 0, 0 };
+
+      solution.MoveZeroes(ref notSortedNums);
+
+      CollectionAssert.AreEqual(notSortedNums, sortedNums);
+    }
+
+    [TestMethod]
+    public void SmallZerosTest()
+    {
+      int[] notSortedNums = { 0 };
+      int[] sortedNums = { 0 };
+
+      solution.MoveZeroes(ref notSortedNums);
+
+      CollectionAssert.AreEqual(notSortedNums, sortedNums);
+    }
+  }
+}
