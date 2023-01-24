15f209e073bc3e2a6cef94dae2abc3c7b59ad0c2
Daniil Stepyrev <danik-steperev@mail.ru>
2023-01-24 20:57:18 +0300

    Added solution and test for task 234: Palindrome Linked List

---
+++ LeetCodeSolutions/Solutions/[234] Palindrome Linked List/Solution234.cs
@@ -0,0 +1,47 @@
+﻿using LeetCodeSolutions.Solutions.RemoveDuplcatesFromSortedList;
+
+namespace LeetCodeSolutions.Solutions._234__Palindrome_Linked_List
+{
+  public class Solution234
+  {
+    public bool IsPalindrome(ListNode head)
+    {
+      ListNode slowNode = head, fastNode = head, prevNode, tmpNode;
+
+      // After cycle slowNode will be a pointer to the middle node of the list.
+      while (fastNode != null && fastNode.next != null)
+      {
+        slowNode = slowNode.next;
+        fastNode = fastNode.next.next;
+      }
+
+      // Reverse second list half order.
+      prevNode = slowNode;
+      slowNode = slowNode.next;
+      prevNode.next = null;
+      while (slowNode != null)
+      {
+        tmpNode = slowNode.next;
+        slowNode.next = prevNode;
+        prevNode = slowNode;
+        slowNode= tmpNode;
+      }
+
+      // Check list using two pointers.
+      slowNode = prevNode;
+      prevNode = head;
+      while (slowNode != null)
+      {
+        if (prevNode.val != slowNode.val)
+        {
+          return false;
+        }
+
+        slowNode = slowNode.next;
+        prevNode = prevNode.next;
+      }
+
+      return true;
+    }
+  }
+}
---
+++ LeetCodeTests/Tests/[234] Palindrome Linked List/Solution234Tester.cs
@@ -0,0 +1,52 @@
+﻿using LeetCodeSolutions.Solutions._234__Palindrome_Linked_List;
+using LeetCodeSolutions.Solutions.RemoveDuplcatesFromSortedList;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCodeTests.Tests._234__Palindrome_Linked_List
+{
+  [TestClass]
+  public class Solution234Tester : AbstractSolutionTester<Solution234>
+  {
+    [TestMethod]
+    public void PalindromeTest()
+    {
+      ListNode head = new ListNode()
+      {
+        val = 1,
+        next = new ListNode()
+        {
+          val = 2,
+          next = new ListNode()
+          {
+            val = 2,
+            next = new ListNode()
+            {
+              val = 1,
+            }
+          }
+        }
+      };
+
+      bool isMethodPalindrome = solution.IsPalindrome(head);
+
+      Assert.IsTrue(isMethodPalindrome);
+    }
+
+    [TestMethod]
+    public void NotPalindromeTest()
+    {
+      ListNode head = new ListNode()
+      {
+        val = 1,
+        next = new ListNode()
+        {
+          val = 2,
+        }
+      };
+
+      bool isMethodPalindrome = solution.IsPalindrome(head);
+
+      Assert.IsFalse(isMethodPalindrome);
+    }
+  }
+}
